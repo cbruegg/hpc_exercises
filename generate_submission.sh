98a205a7fada0d3583c91f542a98720097cfe105
Christian Brüggemann <mail@cbruegg.com>
2018-12-10 14:45:45 +0100

    New submission script

--- generate_submission.sh
+++ generate_submission.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-zip submission.zip $(git ls-tree -r HEAD --name-only | awk 1 ORS=' ')
+if [[ "$#" -ne 1 ]]; then
+    echo "You need to specify the lab number!"
+    exit 1
+fi
+
+NUM=$1
+PAD_NUM=`printf "%02d" ${NUM}`
+
+ADDITIONAL_FILES=""
+if (($NUM == 6)); then
+    ADDITIONAL_FILES="${ADDITIONAL_FILES} input*.ppm"
+fi
+
+TRACKED=`git ls-tree -r HEAD --name-only | awk 1 ORS=' ' | tr " " "\n" | sort -g`
+LAB_FILES=`echo "ManualMakefile lab${PAD_NUM}* ${ADDITIONAL_FILES}" | tr " " "\n" | sort -g`
+INTERSECTION_FILES=`comm -12 <(echo ${TRACKED} | tr " " "\n") <(echo ${LAB_FILES} | tr " " "\n")`
+
+zip submission_${PAD_NUM}.zip ${INTERSECTION_FILES}
